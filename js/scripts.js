d12e3db591989f412f50d84b9690952ebb085de3
HannahChege <chegehannah45@gmail.com>
2018-07-13 18:35:01 +0300

    indentation in javascript

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,19 @@
-$(document).ready(function(){
-  $("#number").submit(function(event){
+$(document).ready(function() {
+  $("#number").submit(function(event) {
     event.preventDefault();
-    var ping =$(".number").val();
-     //$("#result").text(ping)
-     for(var i=1;i<=ping;i++){
-       if ((i%3===0)&&(i%5===0)){
-       $("ul#results").append("<li> pingpong </li>");
-     }
-     else if(i%3===0){
+    var ping = $(".number").val();
+    //$("#result").text(ping)
+    for (var i = 1; i <= ping; i++) {
+      if ((i % 3 === 0) && (i % 5 === 0)) {
+        $("ul#results").append("<li> pingpong </li>");
+      } else if (i % 3 === 0) {
         $("ul#results").append("<li> ping </li>");
-     }
-       else if (i%5===0) {
-             $("ul#results").append("<li> pong </li>");
-       }
-       else {
-         $("ul#results").append("<li>" + i + "</li>");
-       }
-     };
+      } else if (i % 5 === 0) {
+        $("ul#results").append("<li> pong </li>");
+      } else {
+        $("ul#results").append("<li>" + i + "</li>");
+      }
+    };
 
-});
+  });
 });
