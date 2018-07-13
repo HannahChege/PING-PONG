e82a94a9098441e4b3da3efe0f7c4c830ef90a71
HannahChege <chegehannah45@gmail.com>
2018-07-13 15:24:57 +0300

    for loop

--- js/scripts.js
+++ js/scripts.js
@@ -2,6 +2,9 @@ $(document).ready(function(){
   $("#number").submit(function(event){
     event.preventDefault();
     var ping =$(".number").val();
-     $("#result").text(ping)
+     //$("#result").text(ping)
+     for(var i=1;i<=ping;i++){
+       $("ul#results").append("<li>" + i + "</li>");
+     }
   });
 });
