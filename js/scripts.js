db2ca192e9aa5ce364b70d0f87fd75eb319eb9dc
HannahChege <chegehannah45@gmail.com>
2018-07-13 16:02:54 +0300

    added the else statements

--- js/scripts.js
+++ js/scripts.js
@@ -5,6 +5,20 @@ $(document).ready(function(){
      //$("#result").text(ping)
      for(var i=1;i<=ping;i++){
        $("ul#results").append("<li>" + i + "</li>");
+       if ((i%3===0)&&(i%5===0)){
+       $("ul#results").append("<li> pingpong </li>");
+     }elseif((i&3==0)){
+        $("ul#results").append("<li> ping </li>");
      }
-  });
+       else if ((i&5==0)) {
+             $("ul#results").append("<li> pong </li>");
+
+       }
+       else {
+         $("ul#results").append("<li> i </li>");
+       }
+     };
+
+
+});
 });
