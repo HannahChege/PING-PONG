fe154d8872060de3286f4f2f1d4ab4282aa721ba
HannahChege <chegehannah45@gmail.com>
2018-07-13 14:38:38 +0300

    form and input

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,7 @@
 $(document).ready(function(){
-  $("form#pingpong").submit(function(event){
+  $("#number").submit(function(event){
     event.preventDefault();
-    var number=parseInt($("input#number").val());
-    var result=number(year);
-    $("#result").text(result);
+    var ping =$(".number").val();
+     $("#result").text(ping)
   });
 });
