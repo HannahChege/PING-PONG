91f2a2285dc4c0dd186b07ded808fefcd1ec3eec
HannahChege <chegehannah45@gmail.com>
2018-07-13 17:17:46 +0300

    added if else

--- js/scripts.js
+++ js/scripts.js
@@ -4,21 +4,19 @@ $(document).ready(function(){
     var ping =$(".number").val();
      //$("#result").text(ping)
      for(var i=1;i<=ping;i++){
-       $("ul#results").append("<li>" + i + "</li>");
        if ((i%3===0)&&(i%5===0)){
        $("ul#results").append("<li> pingpong </li>");
-     }elseif((i&3==0)){
+     }
+     else if(i%3===0){
         $("ul#results").append("<li> ping </li>");
      }
-       else if ((i&5==0)) {
+       else if (i%5===0) {
              $("ul#results").append("<li> pong </li>");
-
        }
        else {
-         $("ul#results").append("<li> i </li>");
+         $("ul#results").append("<li>" + i + "</li>");
        }
      };
-
 
 });
 });
